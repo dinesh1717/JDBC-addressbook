63accf2b4cf3b9f2dd44e86a3c5ff16d69ba696d
dinesh nangare <dineshnangare1717@gmail.com>
2022-05-09 01:20:03 +0530

    retriving database

--- src/test/java/com/bridgelabz/addressbookjdbc/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/addressbookjdbc/AddressBookServiceTest.java
@@ -3,7 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.LocalDate;
+
 import java.util.List;
 
 
@@ -19,22 +19,5 @@ public class AddressBookServiceTest {
         List<Person> addressBookData = addressBookService.readAddressBookData(AddressBookService.IOService.DB_IO);
         Assert.assertEquals(3, addressBookData.size());
     }
-
-    @Test
-    public void givenContactNumber_WhenUpdated_ShouldSyncWithDb() {
-        AddressBookService addressBookService = new AddressBookService();
-        addressBookService.updateContactNumber("archana","11111111");
-        boolean result = addressBookService.checkAddressBookInSyncWithDB("archana");
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenDateRangeWhenRetrieved_ShouldMatchEntryCount() {
-        AddressBookService addressBookService = new AddressBookService();
-        LocalDate startDate = LocalDate.of(2017, 01, 01);
-        LocalDate endDate = LocalDate.now();
-        List<Person> addressBookDataList =
-                addressBookService.readAddressBookDateRange(AddressBookService
-                        .IOService.DB_IO, startDate, endDate);
-        Assert.assertEquals(2, addressBookDataList.size());
-    }
 }
+
